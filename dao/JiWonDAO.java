4f2b7ae3fb05edf4b8d12021b96d537adc06f11f
JiwonYeom <jwyeom63@gmail.com>
2017-09-24 20:44:08 +0900

    remove codes that are not mine / unnecessary

--- dao/JiWonDAO.java
+++ dao/JiWonDAO.java
@@ -12,35 +12,25 @@ public interface JiWonDAO {
 	//bookCart DAO들
 	//책 리스트 불러오기
 	public List<BookCart> selectBookCart(int userNo);
-	
 	//카트 아이템 삭제하기
 	public int deleteCart(int cartNo);
-	
-	
 	//카트 아이템 합계
 	public int selectCartSumJW(int userNo);
 	//카트 아이템 총수량
 	public int selectCartCountJW(int userNo);
-	
 	//마지막 주문하기 시 장바구니 업데이트
 	public int updateCartJW(BookCart bookCart);
-	
 	//위시리스트에서 장바구니로 책 더하는 메서드
 	public int insertCartJW(BookCart bookCart);
-	
+
 	///////////////////wishlist DAO들////////////////////
 	//위시리스트 불러오기
 	public List<Book> selectWishlist(Wishlist wishlist);
-	
 	//위시리스트 삭제하기
 	public int deleteWishlistJW(int wishlistNo);
-	
 	//위시리스트 오래된 순으로 불러오기
 	public List<Book> wishlistOldOrderJW(Wishlist wishlist);
-	
-	
-	//bookList DAO들
-
+	//bookList DAO
 	public List<Book> selectListJW(Book book);
 	//전체 책 갯수
 	public int selectTotalJW(Book book);
@@ -50,8 +40,6 @@ public interface JiWonDAO {
 	public int selectAwardTotalJW(int no);
 	//award이름 불러오기
 	public String selectAwardNameJW(int no);
-	
-	
 	//주소 DAO
 	public int updateAddressJW(Address address);
 }
